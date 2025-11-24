f8e0b27f5307023746a4d2d3fa2b4dd5417e9a21
René Barczyk <r.barczyk@mitutoyo.nl>
2025-11-24 13:54:08 +0100

    fix indentation in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/earth_animation/',
-  build: {
-    outDir: 'docs',
-  },
+    base: '/earth_animation/',
+    build: {
+        outDir: 'docs',
+    },
 });
